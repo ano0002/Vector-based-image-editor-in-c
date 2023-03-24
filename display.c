e120f9224d44060191d5a228f19aead98d35a329
Hormone4 <117640003+Hormone4@users.noreply.github.com>
2023-03-24 12:57:33 +0100

    worked on the displeay file

--- display.c
+++ display.c
@@ -1,7 +1,40 @@
-#include "display.h"
-
 #include <stdio.h>
 
+#include "display.h"
 
-void display(Shape ** L,int LS) {
+
+void display(Shape ** L, int SIZE) {
+    char M[5][5];
+    for(int i=0; i<5; i++)  for(int j=0; j<5; j++)  M[i][j] = 'O';
+    char symbol = 'w';
+
+
+
+    for (int i=0; i<SIZE; i++) {
+        Shape *shape = L[i];
+
+        if (shape->shape_type == POINT) {
+            Point *p = (Point *) shape->ptrShape;
+            M[p->y][p->x] = symbol;
+        }
+
+        else if (shape->shape_type == LINE) {}
+        
+        else if (shape->shape_type == CIRCLE) {}
+        
+        else if (shape->shape_type == RECTANGLE) {}
+        
+        else if (shape->shape_type == SQUARE) {}
+        
+        else if (shape->shape_type == POLYGON) {}
+    }
+
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<5; j++) printf("%c", M[i][j]);
+        printf("\n");
+    }
 }
+
+
+
+
--- display.h
+++ display.h
@@ -1,7 +1,8 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
+
 #include "shapes.h"
 
-void display(Shape ** L,int LS);
+void display(Shape ** L, int SIZE);
 
 #endif
